3747d39bc06133c216c5de828de8dfb29acde19a
fxia22 <xf1280@gmail.com>
2018-05-02 18:01:42 -0700

    fix

--- baselines/a2c/utils.py
+++ baselines/a2c/utils.py
@@ -39,19 +39,19 @@ def ortho_init(scale=1.0):
         return (scale * q[:shape[0], :shape[1]]).astype(np.float32)
     return _ortho_init
 
-def conv(x, scope, *, nf, rf, stride, pad='VALID', init_scale=1.0):
+def conv(x, scope, *, nf, rf, stride, pad='VALID', init_scale=1.0, act=tf.nn.relu):
     with tf.variable_scope(scope):
         nin = x.get_shape()[3].value
         w = tf.get_variable("w", [rf, rf, nin, nf], initializer=ortho_init(init_scale))
         b = tf.get_variable("b", [nf], initializer=tf.constant_initializer(0.0))
-        return tf.nn.conv2d(x, w, strides=[1, stride, stride, 1], padding=pad)+b
+        return act(tf.nn.conv2d(x, w, strides=[1, stride, stride, 1], padding=pad)+b)
 
-def fc(x, scope, nh, *, init_scale=1.0, init_bias=0.0):
+def fc(x, scope, nh, *, init_scale=1.0, init_bias=0.0, act=tf.nn.relu):
     with tf.variable_scope(scope):
         nin = x.get_shape()[1].value
         w = tf.get_variable("w", [nin, nh], initializer=ortho_init(init_scale))
         b = tf.get_variable("b", [nh], initializer=tf.constant_initializer(init_bias))
-        return tf.matmul(x, w)+b
+        return act(tf.matmul(x, w)+b)
 
 def batch_to_seq(h, nbatch, nsteps, flat=False):
     if flat:
